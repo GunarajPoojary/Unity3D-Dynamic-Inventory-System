8e1b5fb634023da055ef185537f2fc59294075a8
Gunaraj Poojary <gunarajpoojary579@gmail.com>
2025-09-24 11:56:43 +0530

    Implemented fully fucntional ShopMenu

--- Dynamic-Inventory-System/Assets/Scripts/UI/ShopItemUI.cs
+++ Dynamic-Inventory-System/Assets/Scripts/UI/ShopItemUI.cs
@@ -1,18 +1,23 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class ShopItemUI : MonoBehaviour
 {
-    [SerializeField] private ItemConfigSO _itemConfigSO;
     [SerializeField] private Image _itemImage;
     [SerializeField] private TMP_Text _itemName;
     [SerializeField] private Button _button;
     [SerializeField] private ItemConfigSOEventChannelSO _itemConfigSOEventChannelSO;
-    [Range(1, 9)][SerializeField] private int _quantity = 1;
 
-    private void Awake()
+    private ItemConfigSO _itemConfigSO;
+    private int _quantity = 1;
+
+    public void Initialize(ItemConfigSO itemConfigSO, int quantity = 1)
     {
+        _itemConfigSO = itemConfigSO;
+        _quantity = quantity;
+
         if (_itemConfigSO != null)
         {
             _itemImage.sprite = _itemConfigSO.Icon;
@@ -33,5 +38,7 @@ public class ShopItemUI : MonoBehaviour
     private void OnButtonClick()
     {
         _itemConfigSOEventChannelSO.RaiseEvent(_itemConfigSO, _quantity);
+
+        gameObject.SetActive(false);
     }
 }
--- Dynamic-Inventory-System/Assets/Scripts/UI/ShopMenu.cs
+++ Dynamic-Inventory-System/Assets/Scripts/UI/ShopMenu.cs
@@ -1,9 +1,39 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ShopMenu : MonoBehaviour
 {
+    [Header("Pooling Settings")]
+    [SerializeField] private ShopItemUI _shopItemPrefab;
+    [SerializeField] private Transform _container;
+    [SerializeField] private int _itemsCount = 24;
+
+    [Header("Shop Settings")]
+    [SerializeField] private ItemConfigSO[] _availableItems;
+    private List<ShopItemUI> _activeItems = new List<ShopItemUI>();
+
+    private void Awake()
+    {
+        for (int i = 0; i < _itemsCount; i++)
+        {
+            CreateShopItem();
+        }
+    }
+
+    private ShopItemUI CreateShopItem()
+    {
+        ShopItemUI newItem = Instantiate(_shopItemPrefab, _container);
+        newItem.Initialize(_availableItems[Random.Range(0, _availableItems.Length)], Random.Range(1, 10));
+        _activeItems.Add(newItem);
+        return newItem;
+    }
+
     public void Refresh()
     {
-        
+        foreach (ShopItemUI item in _activeItems)
+        {
+            item.Initialize(_availableItems[Random.Range(0, _availableItems.Length)], Random.Range(1, 10));
+            item.gameObject.SetActive(true);
+        }
     }
 }
